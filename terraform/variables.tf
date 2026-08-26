338b7284467e2e64f7528b2be3b356c7b7b77c0f
Rainzler <yasser458891@gmail.com>
2026-08-26 23:25:44 +0100

    Setup terraform

---
+++ terraform/variables.tf
@@ -0,0 +1,17 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "eu-west-3"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "project_name" {
+  description = "Project name"
+  type        = string
+  default     = "flowforge"
+}
